ca3c0c5e3c9a6908cdaabaf139e5503007de8757
Harold Coronado <harold@blakzr.com>
2026-09-14 19:57:07 -0500

    home: video modal

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: 'Montserrat',
+      cssVariable: '--font-montserrat',
+      weights: [400, 500, 600, 700, 800, 900],
+      styles: ['normal'],
+      fallbacks: ['sans-serif'],
+    },
+  ],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
